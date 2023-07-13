10476ecffba9406c8cf07020ecc1f4cca30fe8a8
Timothy Click <tclick@alumni.ou.edu>
2023-07-13 14:41:44 -0500

    ci: update Python versions
    Use Python 3.10 or 3.11 for nox

--- {{cookiecutter.project_name}}/noxfile.py
+++ {{cookiecutter.project_name}}/noxfile.py
@@ -23,8 +23,8 @@ except ImportError:
 
 
 package = "{{cookiecutter.package_name}}"
-python_versions = ["3.10", "3.9", "3.8", "3.7"]
-nox.needs_version = ">= 2021.6.6"
+python_versions = ["3.11", "3.10"]
+nox.needs_version = ">= 2023.4.22"
 nox.options.sessions = (
     "pre-commit",
     "safety",
