a532a03c6c8c97e52153d3b39f534db66a43b51f
Timothy Click <tclick@alumni.ou.edu>
2023-07-13 14:44:45 -0500

    refactor: Annotate self in class methods
    Annotate self class methods using class name

--- {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/cli.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/cli.py
@@ -16,7 +16,6 @@ Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
 from __future__ import annotations
 
 from pathlib import Path
-from typing import TypeVar
 
 import click
 from click_extra import help_option, version_option
@@ -28,12 +27,10 @@ CONTEXT_SETTINGS = {
     "show_default": True,
 }
 
-TComplexCLI = TypeVar("TComplexCLI", bound="ComplexCLI")
-
 class ComplexCLI(click.Group):
     """Complex command-line options with subcommands for fluctmatch."""
 
-    def list_commands(self: TComplexCLI, ctx: click.Context) -> list[str]:
+    def list_commands(self: ComplexCLI, ctx: click.Context) -> list[str]:
         """List available commands.
 
         Parameters
@@ -54,7 +51,7 @@ class ComplexCLI(click.Group):
         rv.sort()
         return rv
 
-    def get_command(self: TComplexCLI, ctx: click.Context, name: str) -> click.Command | None:
+    def get_command(self: ComplexCLI, ctx: click.Context, name: str) -> click.Command | None:
         """Run the selected command.
 
         Parameters
